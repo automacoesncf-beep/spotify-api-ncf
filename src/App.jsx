a972526aae63954ef329330de3988c6b357ebf7d
Eder Carvalho <edercarvalho674@gmail.com>
2026-02-25 21:22:21 -0300

    Alteração nos arquivos .tsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Dashboard from "./features/dashboard/pages/Dashboard";
 import Login from "./features/login/pages/Login";
@@ -7,17 +7,15 @@ import Schadule from "./features/schadule/pages/Schadule";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" />} />
+    <Routes>
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
 
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/reproduction" element={<Reproduction />} />
-        <Route path="/schadule" element={<Schadule />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/reproduction" element={<Reproduction />} />
+      <Route path="/schadule" element={<Schadule />} />
 
-        <Route path="*" element={<div>404</div>} />
-      </Routes>
-    </BrowserRouter>
+      <Route path="*" element={<div>404</div>} />
+    </Routes>
   );
 }
--- src/features/dashboard/pages/Dashboard.tsx
+++ src/features/dashboard/pages/Dashboard.tsx
@@ -1,19 +1,18 @@
 import { useState } from "react";
 
-export default function Dashboard(){
+export default function Dashboard() {
+  return (
+    <div className="min-h-screen bg-zinc-50 p-6">
+      <div className="mx-auto max-w-xl rounded-2xl border border-zinc-200 bg-white p-4 shadow-sm">
+        <h3 className="text-base font-semibold text-zinc-900">Reprodução</h3>
+        <p className="mt-1 text-sm text-zinc-600">
+          Controle a lista de Reproduçao do Spotify
+        </p>
 
-    return(
-
-    <div className="rounded-2xl border border-zinc-200 bg-white p-4 shadow-sm">
-      <h3 className="text-base font-semibold text-zinc-900">Título do card</h3>
-      <p className="mt-1 text-sm text-zinc-600">
-        Aqui vai um texto/descrição do card.
-      </p>
-
-      <button className="mt-4 rounded-xl bg-zinc-900 px-4 py-2 text-sm font-semibold text-white hover:bg-zinc-800">
-        Ação
-      </button>
+        <button className="mt-4 rounded-xl bg-zinc-900 px-4 py-2 text-sm font-semibold text-white hover:bg-zinc-800">
+          Ação
+        </button>
+      </div>
     </div>
   );
-
 }
